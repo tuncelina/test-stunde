17be78c602ad6a7d918e52b43b67018565d249e1
student <student@158.197.35.79>
2019-09-17 10:35:53 +0200

    vypis ze git je super

--- src/main/java/neviem/test_gitu/App.java
+++ src/main/java/neviem/test_gitu/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Kosice!" );
+        for (int i = 0; i < 5; i++) {
+			System.out.println("Git je super");
+		}
     }
 }
