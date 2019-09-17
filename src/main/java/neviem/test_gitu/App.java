9740ece2adb474bdd075b87710a886f97e15d8d8
student <student@158.197.35.79>
2019-09-17 09:37:33 +0200

    initial commit

---
+++ src/main/java/neviem/test_gitu/App.java
@@ -0,0 +1,13 @@
+package neviem.test_gitu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
